9cf0b37ad2033c17ddf69504347c0f370db74d49
Anson Felder <ansonfelder@DSS05083-ECN-L.localdomain>
2018-04-16 09:31:16 -0400

    Addied login form, error with npm

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase-tools';
+import { Header } from './components/common';
+import { LoginForm } from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeAppe({
+      apiKey: 'AIzaSyAHU_L515DV7VbYkVCq6S6VgwF95zn0FME',
+      authDomain: 'auth-916d9.firebaseapp.com',
+      databaseURL: 'https://auth-916d9.firebaseio.com',
+      projectId: 'auth-916d9',
+      storageBucket: 'auth-916d9.appspot.com',
+      messagingSenderId: '866189907078'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="Authentication"/>
+        <LoginForm />
       </View>
     );
   }
