3deba990057e2ffac1dfcfc4ceaf6621312836f7
afelder09 <afelder09@gmail.com>
2018-08-19 19:02:56 -0400

    Fixed error, remade AUTH App

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import firebase from 'firebase-tools';
 import { Header } from './components/common';
-import { LoginForm } from './components/LoginForm';
+import LoginForm from './components/LoginForm';
+import firebase from 'firebase';
 
 class App extends Component {
-  componentWillMount() {
-    firebase.initializeAppe({
-      apiKey: 'AIzaSyAHU_L515DV7VbYkVCq6S6VgwF95zn0FME',
-      authDomain: 'auth-916d9.firebaseapp.com',
-      databaseURL: 'https://auth-916d9.firebaseio.com',
-      projectId: 'auth-916d9',
-      storageBucket: 'auth-916d9.appspot.com',
-      messagingSenderId: '866189907078'
-    });
-  }
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyAMK4gCS0zfKaWpSTjHkBCJ9335FBh-2SQ",
+            authDomain: "auth2-f9b5e.firebaseapp.com",
+            databaseURL: "https://auth2-f9b5e.firebaseio.com",
+            projectId: "auth2-f9b5e",
+            storageBucket: "auth2-f9b5e.appspot.com",
+            messagingSenderId: "312184100318"
+          });
+    }
 
-  render() {
-    return (
-      <View>
-        <Header headerText="Authentication"/>
-        <LoginForm />
-      </View>
-    );
-  }
-};
+    render() {
+        return(
+            <View>
+                <Header headerText={'Auth App'} />
+                <LoginForm />
+            </View>
+        )
+    }
+}
 
 export default App;
--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Card, CardSection, Button } from './common';
 
-class LoginForm extends Component {
-  render() {
-     return (
-       <Card>
-        <CardSection />
-        <CardSection />
-        <CardSection>
-          <Button>
-            Log in
-          </Button>
-        </CardSection>
-       </Card>
-     );
-  }
+class LoginForm extends Component{
+    render() {
+        return(<Card>
+            <CardSection></CardSection>
+            <CardSection></CardSection>
+            <CardSection>
+                <Button>Login</Button>
+            </CardSection>
+        </Card>
+        )};
 }
 
 export default LoginForm;
